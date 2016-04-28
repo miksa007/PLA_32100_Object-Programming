4096c246039a801443a706d16caa2683a4088b08
miksa <mika.saari@tut.fi>
2016-04-28 08:01:44 +0300

    Loggikka toimii

--- PLA_32100_GIT/src/tentit/Tentti2011_05_05.java
+++ PLA_32100_GIT/src/tentit/Tentti2011_05_05.java
@@ -1,19 +1,44 @@
 package tentit;
 
+import java.util.Scanner;
+
 public class Tentti2011_05_05 {
 
 	public static void main(String[] args) {
-		Hirsipuu hirsipuu=new Hirsipuu();
+		Hirsipuu hirsipuu = new Hirsipuu();
 		hirsipuu.pelaa();
 
 	}
 
 }
-class Hirsipuu{
-	private String Arvattava="DIPLOMI-INSINOORI";
-	private String arvaus="*******-********";
-	public void pelaa(){
-		
-		System.out.println("testi");
+
+class Hirsipuu {
+	private Scanner lukija = new Scanner(System.in);
+	private String Arvattava = "DIPLOMI-INSINOORI";
+	private String arvaus =    "*******-*********";
+
+	public void pelaa() {
+
+		for (int i = 0; i < 10; i++) {
+
+			System.out.println("Anna kirjain");
+			String kirjain = lukija.nextLine();
+			arvaaMerkki(kirjain);
+		}
+	}
+
+	public void arvaaMerkki(String kirjain) {
+		kirjain = kirjain.toUpperCase();
+		char merkki = kirjain.charAt(0);
+		String uusiArvaus = "";
+		for (int i = 0; i < Arvattava.length(); i++) {
+			if (Arvattava.charAt(i) == merkki) {
+				uusiArvaus += merkki;
+			} else {
+				uusiArvaus += arvaus.charAt(i);
+			}
+		}
+		arvaus = uusiArvaus;
+		System.out.println(arvaus);
 	}
 }
